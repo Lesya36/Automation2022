0cb7267f5e0eac582e012fc8a35a01b2c1fb2313
github_username <email_address>
2022-12-05 17:09:35 -0600

    Hw16.3

---
+++ PythonHW/Hw16.3.py
@@ -0,0 +1,10 @@
+clothes = ["socks", "shirt", "skirt", "scarf"]
+
+def insert_element(new_cloth,index=0 ):
+    clothes.insert(index,new_cloth)
+    print(clothes)
+
+insert_element("hat",1)
+insert_element("jacket",-3)
+insert_element("jeans")
+
