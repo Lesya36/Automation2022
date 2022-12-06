ec5b3a5a26b32bfa4b970d4ba0adce9f92e4cc20
github_username <email_address>
2022-12-05 18:09:32 -0600

    Added second part of Hw

--- PythonHW/Hw16.3.py
+++ PythonHW/Hw16.3.py
@@ -1,3 +1,5 @@
+from typing import List
+
 clothes = ["socks", "shirt", "skirt", "scarf"]
 
 def insert_element(new_cloth,index=0 ):
@@ -8,3 +10,13 @@ insert_element("hat",1)
 insert_element("jacket",-3)
 insert_element("jeans")
 
+
+#secondpart
+employee_shift = ["Mike", "Andrew", "Emma", "Kelly", "John", "Brad"]
+def replace_employee(employee_shift, old_employee, new_employee):
+      old_employee.index = employee_shift.index(old_employee)
+      employee_shift.remove(old_employee)
+      employee_shift.insert(old_employee_index, new_employee)
+      print(employee_shift)
+
+replace_employee(employee_shift, "Kelly", "Maria")
