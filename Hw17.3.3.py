bf5aea3f81e81e722be05b034d0df2967945244a
github_username <email_address>
2022-12-17 21:07:08 -0600

    Hw17.3 part 3

---
+++ Hw17.3.3.py
@@ -0,0 +1,5 @@
+rectangle = input("Please enter 'Start'")
+width = float(input("Please enter the width"))
+height = float(input("Please enter the height"))
+area = width * height
+print("Your rectangle area is", area)
