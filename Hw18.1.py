f93bb07c933d713ae60d0c035e900de0bd72cfca
github_username <email_address>
2022-12-21 16:27:17 -0600

    Homework18.1

---
+++ Hw18.1.py
@@ -0,0 +1,27 @@
+# guess the number
+import random
+
+guessesTaken = 0
+
+print('Hello! What is your name?')
+myName = input()
+
+number = random.randint(0, 50)
+print(myName +  "I am thinking of number between 0 and 50")
+
+for guessesTaken in range(7):
+    print("Take a guess")
+    guess = input()
+    guess = int(guess)
+
+    if guess < number:
+        print('The value is too low')
+
+    if guess > number:
+        print('The value is too high')
+
+    if guess != number:
+        continue
+    guessesTaken = str(guessesTaken + 1)
+    print('You guessed correctly' + myName)
+
