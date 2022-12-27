4c2c58005fab7d51394145567ac1b20f27a5f225
github_username <email_address>
2022-12-26 22:44:11 -0600

    Corected Hw

--- Hw18.1.py
+++ Hw18.1.py
@@ -1,13 +1,12 @@
 # guess the number
 import random
-
 guessesTaken = 0
 
 print('Hello! What is your name?')
 myName = input()
 
 number = random.randint(0, 50)
-print(myName +  "I am thinking of number between 0 and 50")
+print(myName + "I am thinking of number between 0 and 50")
 
 for guessesTaken in range(7):
     print("Take a guess")
@@ -20,8 +19,9 @@ for guessesTaken in range(7):
     if guess > number:
         print('The value is too high')
 
-    if guess != number:
-        continue
-    guessesTaken = str(guessesTaken + 1)
-    print('You guessed correctly' + myName)
+    if guess == number:
+        break
+
+guessesTaken = (guessesTaken + 1)
+print('You guessed correctly' + myName)
 
