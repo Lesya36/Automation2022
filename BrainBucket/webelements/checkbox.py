15ffaea1fb56d3facb235c1ba3921dcf88ad2485
github_username <email_address>
2023-02-07 20:20:02 -0600

    checkbox

---
+++ BrainBucket/webelements/checkbox.py
@@ -0,0 +1,24 @@
+from BrainBucket.webelements.UIElement import UIElement as Element
+from selenium.webdriver.support.select import Select
+
+
+# Create Radiobutton/Checkbox class that will be inherited from UIElement class
+# and will have a method to select the radio button/checkbox if it's not selected.
+
+# subscribe to newsletter NO
+#subscribe_btn_no = driver.find_element_by_xpath("//input[@name= 'newsletter' and @value='0']")
+
+class Checkbox(Element):
+    def __init__(self, browser, by, locator):
+        super().__init__(browser, by, locator)
+
+    def select(self):
+        checkbox_element = self.get_element()
+        if not checkbox_element.is_selected():
+            checkbox_element.click()
+
+    def deselect(self):
+        checkbox_element = self.get_element()
+        if checkbox_element.is_selected():
+            checkbox_element.click()
+
