28dd268dd1940a5bf6963e69e7854e5d933a07c0
github_username <email_address>
2023-03-22 20:57:56 -0500

    Json File

---
+++ BrainBucket/utils/json_file_reader.py
@@ -0,0 +1,31 @@
+import json
+
+
+class JsonFileReader:
+    def __init__(self, filename):
+        self.data = None
+
+        with open(filename, 'r') as config_file:
+            self.data = json.load(config_file)
+
+    def get_browser(self):
+        if 'browser' not in self.data.keys():
+            raise Exception("Browser option is not present in the config file")
+        return self.data['browser']
+
+    def get_wait_time(self):
+        if 'wait_time' not in self.data.keys():
+            raise Exception("Wait_time option is not present in the config file")
+        return int(self.data['wait_time'])
+
+    def get_email(self):
+        if 'get_email' not in self.data.keys():
+            raise Exception("Get_email option is not present in the config file")
+        return int(self.data['get_email'])
+
+    def get_password(self, section_name):
+        if 'get password' not in self.data.keys():
+            raise Exception("Get_password option is not present in the config file")
+        return int(self.data['get password''user1'])
+
+
