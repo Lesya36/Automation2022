483d94ccc7a6963ffbc25515d71a7b5567dfcf92
github_username <email_address>
2023-04-22 00:07:12 -0500

    login_page updated

--- BrainBucket/BDDBehave/logintests/login.feature
+++ BrainBucket/BDDBehave/logintests/login.feature
@@ -1,16 +1,33 @@
-
+@wip
 Feature: Login functionality
   Background:
     Given User launch login page
 
+@positive
   Scenario: a user can login using correct email and password
     Given User is not logged in
     When user enters email and password
     And User clicks Login button
     Then The user's profile page will be launched
 
+@negative @enter_email
   Scenario: User can't login without entering password
     Given User is not logged in
     When User enters email
     And User clicks Login button
-    Then Warning will be shown about 'No match for E-Mail Address and/or Password'
+    Then warning is shown 'No match for E-Mail Address and/or Password'
+
+ @negative @skip @enter_password
+ Scenario: User can't login without entering email
+   Given user is not logged in
+   When user enters password
+   And user clicks Login button
+   Then warning is shown 'No match for E-Mail Address and/or Password'
+
+ @positive @password_reset
+  Scenario: User on the login page can reset password with correct email
+   Given user clicks 'Forgotten Password'
+     When user enters email
+     And  user clicks Continue button
+     Then confirmation message is shown 'An email with a confirmation link has been sent your email address'
+
--- BrainBucket/pages/login_page.py
+++ BrainBucket/pages/login_page.py
@@ -9,29 +9,50 @@ class LoginPage:
         self.header = Header(browser)
         self.right_menu = RightMenu(browser)
         self.continue_btn = Element(browser, By.XPATH, "//a[contains(text(), 'Continue')]")
-        self.input_email = Element(browser, By.XPATH, "//input[@id='input-email']")
-        self.input_password = Element(browser, By.XPATH, " //input[@id='input-password']")
-        self.login_btn = Element(browser, By.ID, "Login")
+        self.new_customer_title = Element(browser, By.XPATH, "//h2[contains(text(), 'New Customer')]")
+        self.returning_customer_title = Element(browser, By.XPATH, "//h2[contains(text(), 'Returning Customer')]")
+        self.email_input = Element(browser, By.ID, "input-email")
+        self.password_input = Element(browser, By.ID, "input-password")
+        self.login_button = Element(browser, By.XPATH, "//input[@value='Login']")
+        self.forgotten_btn = Element(browser, By.XPATH, "//a[contains(text(), 'Forgotten Password'")
+        self.warning_title = Element(browser, By.XPATH, "//div[contains(text(), 'Warning: No match')]")
+        self.confirmation_message = Element(browser, By.XPATH, "//div[contains(text(), 'An email with confirmation'")
 
     def open_registration_from_menu(self):
-        self.header.open_login_page()
+        self.header.open_login_btn()
         self.right_menu.click_registration()
 
     def open_registration_from_account_dropdown(self):
         self.header.open_registration_form()
 
     def click_continue_btn(self):
-        self.header.open_login_page()
+        self.header.open_login_btn()
         self.continue_btn.click()
 
-    def input_email(self, text):
-        self.header.open_login_page()
-        self.input_email.enter_text(text)
+    def get_new_customer_title(self):
+        return self.new_customer_title.get_text()
 
-    def input_password(self, text):
-        self.header.open_login_page()
-        self.input_password.enter_text(text)
+    def get_returning_customer_title(self):
+        return self.returning_customer_title.get_text()
 
-    def login_btn(self):
-        self.header.open_login_page()
-        self.login_btn.click()
+    def enter_email(self, text):
+        self.header.open_login_btn()
+        self.email_input.enter_text(text)
+
+    def enter_password(self, text):
+        self.header.open_login_btn()
+        self.password_input.enter_text(text)
+
+    def click_login_button(self):
+        self.header.open_login_btn()
+        self.login_button.click()
+
+    def click_forgotten_btn(self):
+        self.header.open_login_btn()
+        self.forgotten_btn.click()
+
+    def get_warning_title(self):
+        return self.warning_title.get_text()
+
+    def get_confirmation_message(self):
+        return self.confirmation_message.get_text()
