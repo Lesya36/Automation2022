05d4cf81b768729d56358179982f42849372a2db
github_username <email_address>
2023-05-22 22:02:55 -0500

    git add

--- BrainBucket/utils/remote_config_reader.py
+++ BrainBucket/utils/remote_config_reader.py
@@ -4,49 +4,19 @@ from BrainBucket.utils.json_file_reader import JsonFileReader
 class RemoteConfigReader(JsonFileReader):
     def __init__(self, filename):
         super().__init__(filename)
-        self.data = None
 
+    def get_username(self):
+        if 'username' in self.data.keys():
+            raise Exception("username option is not found in config file")
+        return self.data['username']
 
-remote_reader = RemoteConfigReader("BrainBucket/webelements/remote_config.json")
+    def get_access_key(self):
+        if 'access_key' not in self.data.keys():
+            raise Exception("access_key option is not found in config file")
+        return self.data['access_key']
 
+    def get_desired_cap(self):
+        if 'desired_cap' not in self.data.keys():
+            raise Exception("desired_cap option is not present in the config file")
+        return self.data['desired_cap']
 
-def get_username(self):
-    if 'username' in self.data.keys():
-        raise Exception("username option is not found in config file")
-    return self.data['username']
-
-
-def get_access_key(self):
-    if 'access_key' not in self.data.keys():
-        raise Exception("access_key option is not found in config file")
-    return self.data['access_key']
-
-
-def get_browser(self):
-    if 'browser' not in self.data.keys():
-        raise Exception("browser option is not present in the config file")
-    return self.data['browser']
-
-
-def get_browser_version(self):
-    if 'browser_version' not in self.data.keys():
-        raise Exception("browser version option is not present in the config file")
-    return self.data['browser_version']
-
-
-def get_os(self):
-    if 'os' not in self.data.keys():
-        raise Exception("os option is not present in the config file")
-    return self.data['os']
-
-
-def get_os_version(self):
-    if 'os_version' not in self.data.keys():
-        raise Exception("os_version option is not present in the config file")
-    return self.data['os_version']
-
-
-def get_name(self):
-    if 'name' not in self.data.keys():
-        raise Exception("name option is not present in the config file")
-    return self.data['name']
--- BrainBucket/webelements/browser.py
+++ BrainBucket/webelements/browser.py
@@ -1,4 +1,4 @@
-import self as self
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import WebDriverException
@@ -6,13 +6,13 @@ from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
-from BrainBucket.utils.remote_config_reader import remote_reader, RemoteConfigReader
+from BrainBucket.utils.remote_config_reader import RemoteConfigReader
 
 
 class Browser:
     def __init__(self, url, browser_name="", time_wait=10):
         # decide which browser to open, can be extended
-        global BROWSERSTACK_URL
+
         if browser_name.lower() == "firefox":
             options = webdriver.FirefoxOptions()
             options.add_argument("--width=360")
@@ -32,29 +32,14 @@ class Browser:
             options.add_experimental_option("excludeSwitches", ['enable-automation'])
             self.driver = webdriver.Chrome(executable_path=r'drivers/chromedriver', options=options)
         elif browser_name.lower() == 'remote':
-            BROWSERSTACK_URL = 'https://lesyakhmelovska_OmdNk5:4EHX7ULrbop9NesbwiBe@hub-cloud.browserstack.com/wd/hub'
-            desired_cap = {
+            remote_reader = RemoteConfigReader("BrainBucket/webelements/remote_config.json")
+            user_name = remote_reader.get_username()
+            access_key = remote_reader.get_access_key()
 
-                'os': 'Windows',
-                'os_version': '10',
-                'browser': 'Chrome',
-                'browserVersion': 'latest',
-                "username": "lesyakhmelovska_OmdNk5",
-                "accesskey": "4EHX7ULrbop9NesbwiBe"
+            BROWSERSTACK_URL = 'https://' + user_name + ':' + access_key + '@hub-cloud.browserstack.com/wd/hub'
+            desired_cap = remote_reader.get_desired_cap()
 
-            }
-
-        elif remote_reader() == RemoteConfigReader("BrainBucket/webelements/remote_config.json")
-            username:remote_reader.get_username
-            access_key:remote_reader.get_access_key
-            browser:remote_reader.get_browser
-            browser_version:remote_reader.get_broweserversion
-            os:remote_reader.get_os
-            os_version:remote_reader.get_os_version
-            name:remote_reader.get_name
-
-
-        self.driver = webdriver.Remote(command_executor=BROWSERSTACK_URL, desired_capabilities=desired_cap)
+            self.driver = webdriver.Remote(command_executor=BROWSERSTACK_URL, desired_capabilities=desired_cap)
 
         self.driver.get(url)
         self.wait = WebDriverWait(self.driver, 10)
