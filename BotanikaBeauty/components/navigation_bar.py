a8829219189e95735d35abafdf29d2e681be7008
github_username <email_address>
2023-06-12 22:44:49 -0500

    fixed

--- BotanikaBeauty/components/navigation_bar.py
+++ BotanikaBeauty/components/navigation_bar.py
@@ -1,31 +1,20 @@
-
 from BrainBucket.SmokeTest import browser
 from BrainBucket.webelements.UIElement import UIElement as Element
 from selenium.webdriver.common.by import By
 
 from BrainBucket.webelements.actions import Actions
-from BrainBucket.webelements.dropdown import Dropdown
-from BrainBucket.webelements.browser import Browser
-
 
 
 class NavigationBar:
-    def __init__(self, browser):
+    def __init__(self):
         self.actions = Actions(browser)
-
         self.shop = Element(browser, By.XPATH, "//span[contains(text(),'Shop')]")
         self.cart = Element(browser, By.XPATH, "//span[@title='Shopping Cart']")
 
-    # Dropdowns
+        # Dropdowns
 
-        self.new_product_dropdown = Dropdown(browser, By.XPATH, "//a[contains(text(), 'NEW Products')]")
+        self.new_product_option = Element(browser, By.XPATH, "//a[contains(text(), 'NEW Products')]")
 
-    def shop_new_products(self):
-        self.actions.move_to_element(self.new_product_dropdown)
-        self.new_product_dropdown.click()
-
-
-
-    #def cart.update(self)
-
-    #def cart.checkout(self)
+    def open_shop_new_products(self):
+        self.actions.move_to_element(self.shop)
+        self.new_product_option.click()
